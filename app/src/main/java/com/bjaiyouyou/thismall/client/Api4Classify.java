1be48c59513387577b3e90326b804258cba717a9
quxinhang <969604386@qq.com>
2017-03-29 07:43:23 +0000

    1、添加一级分类相关内容

--- app/src/main/java/com/bjaiyouyou/thismall/client/Api4Classify.java
+++ app/src/main/java/com/bjaiyouyou/thismall/client/Api4Classify.java
@@ -3,11 +3,9 @@ package com.bjaiyouyou.thismall.client;
 import com.bjaiyouyou.thismall.callback.DataCallback;
 import com.bjaiyouyou.thismall.fragment.ClassifyDetailFragment;
 import com.bjaiyouyou.thismall.fragment.ClassifyPage;
-import com.bjaiyouyou.thismall.fragment.TaskPage;
 import com.bjaiyouyou.thismall.model.ClassifyCateAdModel;
+import com.bjaiyouyou.thismall.model.ClassifyOneCateModel;
 import com.bjaiyouyou.thismall.model.ClassifyTwoCateModel;
-import com.bjaiyouyou.thismall.model.SignInInfo;
-import com.bjaiyouyou.thismall.user.CurrentUserManager;
 import com.bjaiyouyou.thismall.utils.LogUtils;
 
 /**
@@ -43,6 +41,23 @@ public class Api4Classify extends BaseClientApi {
         LogUtils.d(TAG, "getCateAd: " + url);
 
         doGet(url, ClassifyDetailFragment.TAG, null, callback);
+    }
+
+
+    /**
+     * 根据一级分类获取二级分类项列表
+     *
+     * @param callback
+     */
+    public void getOneLevelCate(DataCallback<ClassifyOneCateModel> callback) {
+        //一级分类地址
+        // https://testapi2.bjaiyouyou.com/api/v1/category/getAll
+
+        String url = ClientAPI.API_POINT + "api/v1/category/getAll" ;
+
+        LogUtils.d(TAG, "getOneLevelCate: " + url);
+
+        doGet(url, ClassifyPage.TAG, null, callback);
     }
 
 }
--- app/src/main/java/com/bjaiyouyou/thismall/fragment/ClassifyPage.java
+++ app/src/main/java/com/bjaiyouyou/thismall/fragment/ClassifyPage.java
@@ -1,25 +1,71 @@
 package com.bjaiyouyou.thismall.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.bjaiyouyou.thismall.R;
+import com.bjaiyouyou.thismall.callback.DataCallback;
+import com.bjaiyouyou.thismall.client.Api4Classify;
+import com.bjaiyouyou.thismall.client.ClientApiHelper;
+import com.bjaiyouyou.thismall.model.ClassifyOneCateModel;
+import com.bjaiyouyou.thismall.utils.LogUtils;
+import com.bjaiyouyou.thismall.utils.ToastUtils;
+import com.bjaiyouyou.thismall.utils.UNNetWorkUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
 
 /**
  * 分类页
- *
  * Created by kanbin on 2017/3/28.
+ */
+/**
+ *author Qxh
+ *created at 2017/3/29 10:47
+ *添加一级分类
  */
 public class ClassifyPage extends BaseFragment {
     public static final String TAG = ClassifyPage.class.getSimpleName();
 
+    //布局填充器
+    private LayoutInflater inflater;
+    //一级菜单栏的条目
+    private ScrollView scrollView;
+    //一级菜单栏，条目布局数组
+    private View[] views;
+    //一级菜单栏，标题控件数组
+    private TextView[] tvList;
+    //一级菜单栏，选中标记数组
+    private ImageView[] ivList;
+
+    //一级菜单栏的数据
+    private List<ClassifyOneCateModel.OneCateListBean> list;
+    //当前选中的一级菜单项
+    private int currentItem = 0;
+    //菜单选项对应的内容容器
+    private ViewPager viewpager;
+    //菜单选中内容适配器
+    private ShopAdapter shopAdapter;
+
     protected View layout;
+
+    private Api4Classify mApi4Client;
+
+    private  int mOneCateId=-1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,8 +73,9 @@ public class ClassifyPage extends BaseFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        inflater = LayoutInflater.from(getContext());
+        this.inflater=inflater;
         layout = inflater.inflate(R.layout.fragment_classify, container, false);
         return layout;
     }
@@ -37,24 +84,182 @@ public class ClassifyPage extends BaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initView();
-        setupView();
-        initCtrl();
         loadData();
+
+
     }
 
     private void initView() {
+        viewpager = (ViewPager) layout.findViewById(R.id.viewpager_goods_classify);
+        scrollView = (ScrollView)layout.findViewById(R.id.scrollview_level_one_classify);
 
     }
 
     private void setupView() {
+        viewpager.setOnPageChangeListener(onPageChangeListener);
 
     }
 
     private void initCtrl() {
-
+        shopAdapter = new ShopAdapter(getFragmentManager());
+        viewpager.setAdapter(shopAdapter);
     }
 
     private void loadData() {
+        list=new ArrayList<>();
+        mApi4Client= (Api4Classify) ClientApiHelper.getInstance().getClientApi(Api4Classify.class);
+        mApi4Client.getOneLevelCate(new DataCallback<ClassifyOneCateModel>(getContext()) {
+            @Override
+            public void onFail(Call call, Exception e, int id) {
+                UNNetWorkUtils.unNetWorkOnlyNotify(getContext(), e);
+            }
 
+            @Override
+            public void onSuccess(Object response, int id) {
+                if (response!=null){
+                    ClassifyOneCateModel classifyOneCateModel= (ClassifyOneCateModel) response;
+                    list=classifyOneCateModel.getOneCateList();
+                    LogUtils.e("一级标题的数目",list.size()+"");
+                    showToolsView();
+                    setupView();
+                    initCtrl();
+                }
+            }
+        });
     }
+    /**
+     * 动态生成显示items中的textview
+     */
+    private void showToolsView() {
+        //获取一级菜单数据
+//        list = Model.toolsList;
+        LinearLayout toolsLayout = (LinearLayout)layout.findViewById(R.id.ll_level_one_classify);
+        if (list!=null){
+            tvList = new TextView[list.size()];
+            ivList =new ImageView[list.size()];
+            views = new View[list.size()];
+
+            for (int i = 0; i < list.size(); i++) {
+                View view = inflater.inflate(R.layout.item_level_one_classify, null);
+                view.setId(i);
+                view.setOnClickListener(toolsItemListener);
+                TextView textView = (TextView) view.findViewById(R.id.text);
+                ImageView imageView= (ImageView) view.findViewById(R.id.line);
+                ClassifyOneCateModel.OneCateListBean oneCateListBean=list.get(i);
+                if (oneCateListBean!=null){
+                    String cateName=oneCateListBean.getCate_name();
+                    if (TextUtils.isEmpty(cateName))
+                    textView.setText(cateName);
+
+                    toolsLayout.addView(view);
+                    LogUtils.e("oneCateName",""+cateName);
+                    tvList[i] = textView;
+                    ivList[i]=imageView;
+                    views[i] = view;
+                    mOneCateId=oneCateListBean.getId();
+                }
+            }
+            changeTextColor(0);
+        }
+    }
+
+    /**
+     * 菜单条目点击事件，控制内容切换
+     */
+    private View.OnClickListener toolsItemListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            viewpager.setCurrentItem(v.getId());
+            ToastUtils.showLong(""+list.get(v.getId()).getCate_name());
+        }
+    };
+
+    /**
+     * OnPageChangeListener<br/>
+     * 监听ViewPager选项卡变化事的事件
+     */
+    private ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageSelected(int arg0) {
+            if (viewpager.getCurrentItem() != arg0)
+                viewpager.setCurrentItem(arg0);
+            if (currentItem != arg0) {
+                changeTextColor(arg0);
+                changeTextLocation(arg0);
+            }
+            currentItem = arg0;
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+    };
+
+
+
+    /**
+     * ViewPager 加载选项卡
+     *
+     * @author Administrator
+     *
+     */
+    private class ShopAdapter extends FragmentPagerAdapter {
+        public ShopAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int index) {
+            Fragment fragment = new ClassifyDetailFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("OneCateId", mOneCateId);
+            fragment.setArguments(bundle);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return list==null?0:list.size();
+        }
+    }
+
+    /**
+     * 改变textView的颜色
+     *
+     * @param id
+     */
+    private void changeTextColor(int id) {
+        for (int i = 0; i < tvList.length; i++) {
+            //非选中条目
+            if (i != id) {
+                tvList[i].setBackgroundColor(0x00000000);
+//                tvList[i].setTextColor(0x222222);
+                tvList[i].setTextColor(0xFF000000);
+//                tvList[i].setTextSize(14);
+                ivList[i].setVisibility(View.INVISIBLE);
+            }
+        }
+        //选中条目
+        tvList[id].setBackgroundColor(0xFFFFFFFF);
+//        tvList[id].setTextColor(0xe10c28);
+        tvList[id].setTextColor(0xFFFF5D5E);
+//        tvList[id].setTextSize(17);
+        ivList[id].setVisibility(View.VISIBLE);
+        mOneCateId=list.get(id).getId();
+    }
+
+    /**
+     * 改变栏目位置
+     *
+     * @param clickPosition
+     */
+    private void changeTextLocation(int clickPosition) {
+        int x = (views[clickPosition].getTop());
+        scrollView.smoothScrollTo(0, x);
+    }
+
 }
