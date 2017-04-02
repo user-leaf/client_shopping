536e02ac0f58e7d144e66dba8b4b42eff38c7390
kanbin <kanbin@live.com>
2017-04-02 03:18:29 +0000

    敏感词修改

--- app/src/main/java/com/bjaiyouyou/thismall/activity/WithdrawActivity.java
+++ app/src/main/java/com/bjaiyouyou/thismall/activity/WithdrawActivity.java
@@ -370,7 +370,7 @@ public class WithdrawActivity extends BaseActivity implements View.OnClickListen
                     Toast.makeText(this, "请输入换取数额", Toast.LENGTH_SHORT).show();
                 }else {
                     if (!isCanWithDraw) {
-                        Toast.makeText(this, "输入金额不符合条件，请确认后重新输入", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "输入数额不符合条件，请确认后重新输入", Toast.LENGTH_SHORT).show();
                         return;
                     }else {
                         getWithdraw();
