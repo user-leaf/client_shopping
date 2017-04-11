5bdad736bc17969cd28139b5229a551147b6af80
kanbin <kanbin@live.com>
2017-04-11 03:05:13 +0000

    手机联系人处理

--- app/src/main/java/com/bjaiyouyou/thismall/utils/MobileUtils.java
+++ app/src/main/java/com/bjaiyouyou/thismall/utils/MobileUtils.java
@@ -22,32 +22,23 @@ import java.util.List;
 public class MobileUtils {
     private static final String TAG = MobileUtils.class.getSimpleName();
 
+    private Context mContext = null;
     private List<ContactModel> mMembersEntityList = new ArrayList<>();
 
-    private Context mContext = null;
+    private static final String[] PHONES_PROJECTION = new String[]{     // 获取库Phon表字段
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER,
+            ContactsContract.CommonDataKinds.Photo.PHOTO_ID,
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID};
+    private static final int PHONES_DISPLAY_NAME_INDEX = 0;             // 联系人显示名称
+    private static final int PHONES_NUMBER_INDEX = 1;                   // 电话号码
+    private static final int PHONES_PHOTO_ID_INDEX = 2;                 // 头像ID
+    private static final int PHONES_CONTACT_ID_INDEX = 3;               // 联系人的ID
 
-    /**获取库Phon表字段**/
-    private static final String[] PHONES_PROJECTION = new String[] {
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Photo.PHOTO_ID, ContactsContract.CommonDataKinds.Phone.CONTACT_ID };
+    private CharacterParser characterParser;    // 汉字转换成拼音的类
 
-    /**联系人显示名称**/
-    private static final int PHONES_DISPLAY_NAME_INDEX = 0;
-
-    /**电话号码**/
-    private static final int PHONES_NUMBER_INDEX = 1;
-
-    /**头像ID**/
-    private static final int PHONES_PHOTO_ID_INDEX = 2;
-
-    /**联系人的ID**/
-    private static final int PHONES_CONTACT_ID_INDEX = 3;
-
-    /**
-     * 汉字转换成拼音的类
-     */
-    private CharacterParser characterParser;
-
-    private MobileUtils(){}
+    private MobileUtils() {
+    }
 
     public MobileUtils(Context context) {
         mContext = context;
@@ -56,21 +47,20 @@ public class MobileUtils {
         characterParser = CharacterParser.getInstance();
     }
 
-    /**得到手机通讯录联系人信息**/
+    /**
+     * 得到手机通讯录联系人信息
+     **/
     private void getPhoneContacts() {
         ContentResolver resolver = mContext.getContentResolver();
 
         // 获取手机联系人
-        Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,PHONES_PROJECTION, null, null, null);
+        Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, PHONES_PROJECTION, null, null, null);
 
         if (phoneCursor != null) {
             while (phoneCursor.moveToNext()) {
 
                 //得到手机号码
                 String phoneNumber = phoneCursor.getString(PHONES_NUMBER_INDEX);
-                //当手机号码为空的或者为空字段 跳过当前循环
-                if (TextUtils.isEmpty(phoneNumber))
-                    continue;
 
                 //得到联系人名称
                 String contactName = phoneCursor.getString(PHONES_DISPLAY_NAME_INDEX);
@@ -95,6 +85,10 @@ public class MobileUtils {
 
                 phoneNumber = dealPhoneNumber(phoneNumber);
 
+                if (TextUtils.isEmpty(phoneNumber)) {
+                    continue;
+                }
+
                 ContactModel model = new ContactModel();
                 model.setName(contactName);
                 model.setTel(phoneNumber);
@@ -105,15 +99,15 @@ public class MobileUtils {
                     pinyin = characterParser.getSelling(contactName);
                 }
                 // 防止下一句报错 java.lang.StringIndexOutOfBoundsException: length=0; regionStart=0; regionLength=1
-                if (pinyin.length()<1){
+                if (pinyin.length() < 1) {
                     return;
                 }
                 String sortString = pinyin.substring(0, 1).toUpperCase();
 
                 // 正则表达式，判断首字母是否是英文字母
-                if(sortString.matches("[A-Z]")){
+                if (sortString.matches("[A-Z]")) {
                     model.setSortLetters(sortString.toUpperCase());
-                }else{
+                } else {
                     model.setSortLetters("#");
                 }
 
@@ -126,18 +120,23 @@ public class MobileUtils {
 
     /**
      * 处理手机号码格式，去掉+86、减号等字符
+     *
      * @param str
      * @return
      */
     private String dealPhoneNumber(String str) {
         String ret = null;
-        if (str.startsWith("+86")){
+        if (TextUtils.isEmpty(str)) {
+            return ret;
+        }
+
+        if (str.startsWith("+86")) {
             str = str.substring(3);
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (c>='0'&&c<='9'){
+            if (c >= '0' && c <= '9') {
                 sb.append(c);
             }
         }
@@ -147,7 +146,7 @@ public class MobileUtils {
 
     public List<ContactModel> getAllContacts() {
         getPhoneContacts();
-        Log.d(TAG, "getAllContacts: "+mMembersEntityList.toString());
+        Log.d(TAG, "getAllContacts: " + mMembersEntityList.toString());
         return mMembersEntityList;
     }
 }
