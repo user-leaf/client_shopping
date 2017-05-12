29d9ba70ebedcd130b89ce9ace0c2767b85880cd
kanbin <kanbin@live.com>
2017-05-12 07:44:34 +0000

    用户支付页面自定义金额

--- app/src/main/java/com/bjaiyouyou/thismall/activity/UserPayActivity.java
+++ app/src/main/java/com/bjaiyouyou/thismall/activity/UserPayActivity.java
@@ -18,10 +18,18 @@ import com.bjaiyouyou.thismall.utils.DialogUtils;
  */
 public class UserPayActivity extends Activity implements View.OnClickListener {
 
-    private ImageView mIvHeadImage; // 头像
-    private TextView mTvName;       // 姓名
-    private TextView mTvMoney;      // 金额
-    private Button mBtnPay;         // 支付按钮
+    private ImageView mIvHead;  // 头像
+    private TextView mTvName;   // 姓名
+
+    // 固定金额支付
+    private View mLlPayBanner; // 支付栏
+    private TextView mTvMoney;          // 固定金额
+    private Button mBtnPay;             // 支付按钮
+
+    // 自定义金额支付
+    private View mLlPayBannerCustomMoney;  // 支付栏
+    private EditText mEtMoney;              // 金额输入框
+    private Button mBtnPayCustomMoney;     // 支付按钮
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,47 +38,83 @@ public class UserPayActivity extends Activity implements View.OnClickListener {
 
         initView();
         setupView();
+        showBanner(1);
     }
 
     private void initView() {
-        mIvHeadImage = (ImageView) findViewById(R.id.user_pay_iv_head);
+        mIvHead = (ImageView) findViewById(R.id.user_pay_iv_head);
         mTvName = (TextView) findViewById(R.id.user_pay_tv_name);
+
+        mLlPayBanner = findViewById(R.id.user_pay_ll);
         mTvMoney = (TextView) findViewById(R.id.user_pay_tv_money);
         mBtnPay = (Button) findViewById(R.id.user_pay_btn_pay);
+
+        mLlPayBannerCustomMoney = findViewById(R.id.user_pay_ll_custom_money);
+        mEtMoney = (EditText) findViewById(R.id.user_pay_et_money);
+        mBtnPayCustomMoney = (Button) findViewById(R.id.user_pay_btn_pay_custom_money);
     }
 
     private void setupView() {
         mBtnPay.setOnClickListener(this);
+        mBtnPayCustomMoney.setOnClickListener(this);
+    }
+
+    private void showBanner(int i) {
+        switch (i) {
+            case 1:
+                mLlPayBanner.setVisibility(View.VISIBLE);
+                mLlPayBannerCustomMoney.setVisibility(View.GONE);
+                break;
+            case 2:
+                mLlPayBanner.setVisibility(View.GONE);
+                mLlPayBannerCustomMoney.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.user_pay_btn_pay:
-                // 支付弹窗
-                View inflate = LayoutInflater.from(this).inflate(R.layout.layout_user_pay, null);
-                final Dialog payDialog = DialogUtils.createRandomDialog(this, null, null, null, null, null,
-                        inflate
-                );
+            case R.id.user_pay_btn_pay: // 固定金额支付
+                showPayDialog(123);
+                break;
 
-                View ivClose = inflate.findViewById(R.id.user_pay_dialog_iv_close);
-                EditText etSafecode = (EditText) inflate.findViewById(R.id.user_pay_dialog_et_psw);
-                View.OnClickListener onClickListener = new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        switch (v.getId()) {
-                            case R.id.user_pay_dialog_iv_close:
-                                if (payDialog != null && payDialog.isShowing()) {
-                                    payDialog.dismiss();
-                                }
-                                break;
-                        }
-                    }
-                };
-                ivClose.setOnClickListener(onClickListener);
-
-                payDialog.show();
+            case R.id.user_pay_btn_pay_custom_money:    // 自定义金额支付
+                showPayDialog(321);
                 break;
         }
+    }
+
+    /**
+     * 支付弹窗
+     * @param money 金额
+     */
+    private void showPayDialog(double money) {
+        View inflate = LayoutInflater.from(this).inflate(R.layout.layout_user_pay, null);
+        final Dialog payDialog = DialogUtils.createRandomDialog(this, null, null, null, null, null,
+                inflate
+        );
+
+        View ivClose = inflate.findViewById(R.id.user_pay_dialog_iv_close);
+        TextView tvMoney = (TextView) inflate.findViewById(R.id.user_pay_dialog_tv_money);
+        EditText etSafecode = (EditText) inflate.findViewById(R.id.user_pay_dialog_et_psw);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.user_pay_dialog_iv_close:
+                        if (payDialog != null && payDialog.isShowing()) {
+                            payDialog.dismiss();
+                        }
+                        break;
+                }
+            }
+        };
+
+        tvMoney.setText(String.valueOf(money));
+        ivClose.setOnClickListener(onClickListener);
+
+        payDialog.show();
     }
 }
