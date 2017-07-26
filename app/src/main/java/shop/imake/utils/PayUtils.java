06a4512bf120775c53f082937eac3ba8f86ab5fd
kanbin <kanbin@live.com>
2017-07-26 10:12:33 +0000

    添加PayAlert()

--- app/src/main/java/shop/imake/utils/PayUtils.java
+++ app/src/main/java/shop/imake/utils/PayUtils.java
@@ -1,9 +1,30 @@
 package shop.imake.utils;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.view.View;
 
+import com.bigkoo.alertview.AlertView;
+import com.bigkoo.alertview.OnItemClickListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Call;
+import shop.imake.Constants;
+import shop.imake.client.Api4Cart;
+import shop.imake.client.Api4Task;
+import shop.imake.client.ClientApiHelper;
 import shop.imake.fragment.PayDetailFragment;
+import shop.imake.task.PaymentTask;
+import shop.imake.widget.LoadingDialog;
 
 /**
  * Created by Administrator on 2017/4/21.
@@ -14,16 +35,102 @@ public class PayUtils {
 
     /**
      * 支付
+     *
      * @param fragmentManager
-     * @param tag       Fragment的TAG，根据TAG，fragmentManager可查找到该fragment
-     * @param money     支付金额
+     * @param tag             Fragment的TAG，根据TAG，fragmentManager可查找到该fragment
+     * @param money           支付金额
      */
-    public static void pay(FragmentManager fragmentManager, String tag, double money, PayDetailFragment.PayCallback callback){
+    public static void pay(FragmentManager fragmentManager, String tag, double money, PayDetailFragment.PayCallback callback) {
         PayDetailFragment payDetailFragment = new PayDetailFragment();
         Bundle bundle = new Bundle();
         bundle.putDouble(EXTRA_MONEY, money);
         payDetailFragment.setArguments(bundle);
         payDetailFragment.setOnPayCallback(callback);
         payDetailFragment.show(fragmentManager, tag);
+    }
+
+
+    /**
+     * 底部alert支付
+     * 没有余额支付
+     *
+     * @param context    上下文
+     * @param isActivity 是否是activity
+     * @param activity   环迅支付所需，如果是activity则传activity，如果是fragment则传所在的activity，否则传null
+     * @param fragment   如果是fragment则传fragment，否则传null
+     * @param orderNum   订单号
+     * @param clickView  点击控件，可以传null
+     * @param clazzName  类名
+     */
+    public static void payAlert(final Context context, final boolean isActivity, final Activity activity, final Fragment fragment, final String orderNum, final View clickView, final String clazzName) {
+
+        // 构建支付方式列表
+        final List<String> payWayNames = new ArrayList<>();
+        final Map<String, String> payWayMap = new HashMap<String, String>();
+
+        String strAlipay = "支付宝支付";
+        payWayNames.add(strAlipay);
+        payWayMap.put(strAlipay, Constants.CHANNEL_ALIPAY);
+        String strWechat = "微信支付";
+        payWayNames.add(strWechat);
+        payWayMap.put(strWechat, Constants.CHANNEL_WECHAT);
+
+        String[] payWays = payWayNames.toArray(new String[payWayNames.size()]);
+
+        // 弹出选择框
+        OnItemClickListener onItemClickListener = new OnItemClickListener() {
+            @Override
+            public void onItemClick(Object o, int position) {
+                if (position < 0) {
+                    return;
+                }
+
+                String strPayWay = payWayMap.get(payWayNames.get(position));
+
+                int amount = 1; // 金额 接口已修改，不从此处判断订单金额，此处设置实际无效
+                String channel = null;
+
+                if ("alipay".equals(strPayWay)) {    // 支付宝支付
+                    channel = Constants.CHANNEL_ALIPAY;
+
+                } else if ("wx".equals(strPayWay)) {   // 微信支付
+                    channel = Constants.CHANNEL_WECHAT;
+
+                }
+
+                if (!isActivity) {  // 在fragment中调用
+                    new PaymentTask(
+                            context,
+                            fragment,
+                            orderNum,
+                            channel,
+                            clickView,
+                            clazzName
+                    ).execute(new PaymentTask.PaymentRequest(channel, amount));
+
+                } else {    // 在activity中调用
+                    new PaymentTask(
+                            context,
+                            activity,
+                            orderNum,
+                            channel,
+                            clickView,
+                            clazzName
+                    ).execute(new PaymentTask.PaymentRequest(channel, amount));
+
+                }
+
+            }
+        };
+
+        new AlertView.Builder().setContext(context)
+                .setStyle(AlertView.Style.ActionSheet)
+                .setTitle("请选择支付方式")
+                .setMessage(null)
+                .setCancelText("取消")
+                .setOthers(payWays)
+                .setOnItemClickListener(onItemClickListener)
+                .build()
+                .show();
     }
 }
